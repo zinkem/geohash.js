a3cd1e28003816d5f1dede6d1414f50b67de8171
zinkem <zinkem@geomonkey.com>
2012-03-12 02:54:51 -0700

    Bug fix in geohash, extraneous character

--- files/geohash.js
+++ files/geohash.js
@@ -1,7 +1,7 @@
-`/*
-   Contains javascript functions for coding and decoding geohashes
-
- */
+/*
+  Contains javascript functions for coding and decoding geohashes
+  
+*/
 var base32 = ['0','1','2','3','4','5','6','7',
               '8','9','a','b','c','d','e','f',
               'g','h','i','j','k','l','m','n',
